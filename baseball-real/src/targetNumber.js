761b8963f8493c83d7f2f8cd0c3df6c7b9870410
SeungHwan-Lee <sh981013s@gmail.com>
2022-02-05 21:39:59 +0900

    refactor: 타겟넘버 생성함수 간소화

--- baseball-real/src/targetNumber.js
+++ baseball-real/src/targetNumber.js
@@ -7,16 +7,6 @@ const generateTargetNumber = () => {
   return [...targetNumber];
 };
 
-const isTargetNumberUnique = (tempTarget) => {
-  const uniqueArr = [...new Set(tempTarget.split(''))];
-  return uniqueArr.length === 3;
-};
-
-const isTargetNumberIncludeZero = (tempTarget) => {
-  const regex = /0/g;
-  return regex.test(tempTarget);
-};
-
 export const saveTargetNumber = () => {
   const generatedTargetNumber = generateTargetNumber();
   console.log(generatedTargetNumber.join(''));
